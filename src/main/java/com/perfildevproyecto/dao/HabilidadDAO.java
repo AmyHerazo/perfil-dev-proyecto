160e6b569612f01f97fe256b765fbdae09f06eda
salometoroc-cmd <salome.toroc@udea.edu.co>
2025-11-24 16:55:37 -0500

    interfaces DAO

--- src/main/java/com/perfildevproyecto/dao/HabilidadDAO.java
+++ src/main/java/com/perfildevproyecto/dao/HabilidadDAO.java
@@ -1,1 +1,8 @@
-
+public interface HabilidadDAO{
+    
+    List<Habilidad>listarTodas();
+    void agregar(Habilidad habilidad );
+    void actualizar(Habilidad habilidad );
+    void eliminar(String id);
+    void buscarPorId(String id);
+}
--- src/main/java/com/perfildevproyecto/dao/PerfilDAO.java
+++ src/main/java/com/perfildevproyecto/dao/PerfilDAO.java
@@ -1,1 +1,7 @@
+public interface PerfilDAO{
+    
+    void guardarPerfil(Perfil perfil );
+    void cargarPerfil();
+    void actualizar(Perfil perfil );
+}
 
