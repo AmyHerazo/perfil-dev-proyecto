124ff26393c652f4f947e0cd78e7d75a86cbe88b
salometoroc-cmd <salome.toroc@udea.edu.co>
2025-11-26 14:34:59 -0500

    DAO perfil y habilidades

--- src/main/java/com/perfildevproyecto/dao/impl/PerfilDAOJSON.java
+++ src/main/java/com/perfildevproyecto/dao/impl/PerfilDAOJSON.java
@@ -1,1 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class PerfilDAOJSON implements PerfilDAO {
+     
+    private final String archivo = "perfil.json";
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public void guardarPerfil(Perfil perfil){
+     
+        try {
+            objectMapper.writeValue(new File(archivo), perfil);
+
+         } catch (IOException e) {
+            System.err.println("Error guardando  este perfil: " + e.getMessage());
+            throw new RuntimeException("no se pudo guardar el perfil");
+        }
+        
+    }
+
+    
+    @Override 
+    public Perfil cargarPerfil(){
+        try { 
+            File file = new File(archivo);
+            if (!file.exists()){
+                return null;
+            }
+            return objectMapper.readValue(file, Perfil.class);
+
+        } catch (IOException e) {
+            System.err.println("no se pudo cargar el perfil: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+
+    public void actualizarPerfil(Perfil perfil){
+
+        guardarPerfil(perfil);
+    }
+
+}
